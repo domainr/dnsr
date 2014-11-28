12d88db79507ba0c07ba3efb2c29122e2bca8060
Randy Reddig <ydnar@shaderlab.com>
2014-11-27 17:12:27 -0800

    Worker prototype

--- resolver.go
+++ resolver.go
@@ -9,71 +9,16 @@ type Resolver struct {
 	cache *lru.Cache
 }
 
-func (r *Resolver) Resolve(qname string, qtype dns.Type) <-chan dns.RR {
-	c := make(chan dns.RR, 20)
-	go func() {
-		defer close(c)
-		rrs, ok := r.recall(qname, qtype)
-		if ok {
-			inject(c, rrs)
-			return
-		}
-		rrememberveViaAuthorities(c, qname, qtype)
-	}()
-	return c
-}
-
-func (r *Resolver) resolveViaAuthorities(c chan<- dns.RR, qname string, qtype dns.Type) {
-	pname, ok = parent(qname)
-	if !ok {
-		return
+func (r *Resolver) Resolve(qname string, qtype dns.Type) *Worker {
+	w := &Worker{
+		r: r,
+		qname: qname,
+		qtype: qtype,
+		done: make(chan struct{}),
+		RRs: make(chan dns.RR, 20),
 	}
-	for rr := range r.Resolve(pname, dns.TypeNS) {
-		ns, ok := rr.(*dns.NS)
-		if !ok {
-			continue
-		}
-		rrememberveViaAuthority(c, ns.NS, qname, qtype)
-	}
-}
-
-func (r *Resolver) resolveViaAuthority(c chan<- dns.RR, nsname, qname string, qtype dns.Type) {
-	for rr := range r.Resolve(nsname, dns.TypeA) {
-		a, ok := rr.(*dns.A)
-		if !ok {
-			continue
-		}
-		addr := a.A.String() + ":53"
-		r.exchange(c, addr, qname, qtype)
-	}
-}
-
-func (r *Resolver) resolveViaServer(c chan<- dns.RR, addr, qname string, qtype dns.Type) {
-	msg := &dns.Msg{}
-	msg.SetQuestion(q.Name, q.Qtype)
-	msg.MsgHdr.RecursionDesired = false
-	client := &dns.Client{}
-	rmsg, _, err := client.Exchange(msg, addr)
-	if err != nil {
-		return
-	}
-
-	// FIXME: cache NXDOMAIN responses responsibly
-	if rmsg.Rcode == dns.RcodeNameError {
-		r.rememberNX(qname, qtype)
-	}
-
-	// Cache responses
-	r.remember(rMsg.Answer...)
-	r.remember(rMsg.Ns...)
-	r.remember(rMsg.Extra...)
-
-	// Check cache again
-	rrs, ok := r.recall(q)
-	if ok {
-		inject(c, rrs)
-		return
-	}
+	go w.Start()
+	return w
 }
 
 func (r *Resolver) recall(qname, qtype) ([]dns.RR, bool) {
@@ -87,6 +32,83 @@ func (r *Resolver) remember(rr ...dns.RR) {
 
 func (r *Resolver) rememberNX(qname, qtype) {
 	// FIXME: implement
+}
+
+
+type Worker struct {
+	r     *Resolver
+	qname string
+	qtype dns.Type
+	done  chan struct{}
+	RRs   chan dns.RR
+}
+
+func (w *Worker) Start() {
+	defer w.Stop()
+	rrs, ok := w.r.recall(w.qname, w.qtype)
+	if ok {
+		inject(w.RRs, rrs)
+		return
+	}
+	w.resolveViaAuthorities()
+}
+
+func (w *Worker) Stop() {
+	close(w.done)
+	close(w.RRs)
+}
+
+func (w *Worker) resolveViaAuthorities() {
+	pname, ok = parent(w.qname)
+	if !ok {
+		return
+	}
+	for rr := range w.r.Resolve(pname, dns.TypeNS).RRs {
+		ns, ok := rr.(*dns.NS)
+		if !ok {
+			continue
+		}
+		w.resolveViaAuthority(ns.NS)
+	}
+}
+
+func (w *Worker) resolveViaAuthority(nsname) {
+	for rr := range w.r.Resolve(nsname, dns.TypeA).RRs {
+		a, ok := rr.(*dns.A)
+		if !ok {
+			continue
+		}
+		addr := a.A.String() + ":53"
+		w.resolveViaServer(addr)
+	}
+}
+
+func (w *Worker) resolveViaServer(addr) {
+	msg := &dns.Msg{}
+	msg.SetQuestion(q.Name, q.Qtype)
+	msg.MsgHdr.RecursionDesired = false
+	client := &dns.Client{}
+	rmsg, _, err := client.Exchange(msg, addr)
+	if err != nil {
+		return
+	}
+
+	// FIXME: cache NXDOMAIN responses responsibly
+	if rmsg.Rcode == dns.RcodeNameError {
+		w.r.rememberNX(qname, qtype)
+	}
+
+	// Cache responses
+	w.r.remember(rMsg.Answer...)
+	w.r.remember(rMsg.Ns...)
+	w.r.remember(rMsg.Extra...)
+
+	// Check cache again
+	rrs, ok := w.r.recall(q)
+	if ok {
+		inject(c, rrs)
+		return
+	}
 }
 
 func inject(c chan<- dns.RR, rrs []dns.RR) {
