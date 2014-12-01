0019d3312126a67703e70b54709e9b97faa20c80
Randy Reddig <ydnar@shaderlab.com>
2014-12-01 10:15:11 -0800

    no go-bindata

--- generate.sh
+++ generate.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
-PFX=rootbindata
-DIR=$TMPDIR/$PFX
-mkdir -p $DIR
-curl -o $DIR/root.zone http://www.internic.net/domain/root.zone
-go-bindata -pkg=dnsr -prefix=$DIR -o=root.go $DIR
+echo "package dnsr\n\nvar root = \`" > root.go
+curl http://www.internic.net/domain/named.root >> root.go
+echo "\`" >> root.go
