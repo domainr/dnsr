39afb99c9c03de78e6880a7ea3c6a623a0022713
Randy Reddig <ydnar@shaderlab.com>
2014-12-07 09:36:09 -0800

    remove synchronization function from tests

--- dnsr_test.go
+++ dnsr_test.go
@@ -21,17 +21,17 @@ func init() {
 
 func TestSimple(t *testing.T) {
 	r := New(0)
-	accum(r.Resolve("1.com", ""))
+	r.Resolve("1.com", "")
 }
 
 func TestCache(t *testing.T) {
 	r := New(5)
 	st.Expect(t, r.cache.Len(), 0)
-	accum(r.Resolve("1.com", ""))
-	accum(r.Resolve("2.com", ""))
-	accum(r.Resolve("3.com", ""))
-	accum(r.Resolve("4.com", ""))
-	accum(r.Resolve("5.com", ""))
+	r.Resolve("1.com", "")
+	r.Resolve("2.com", "")
+	r.Resolve("3.com", "")
+	r.Resolve("4.com", "")
+	r.Resolve("5.com", "")
 	st.Expect(t, r.cache.Len(), 5)
 	_ = r.Resolve("6.com", "")
 	st.Expect(t, r.cache.Len(), 5)
@@ -39,7 +39,7 @@ func TestCache(t *testing.T) {
 
 func TestGoogleA(t *testing.T) {
 	r := New(0)
-	rrs := accum(r.Resolve("google.com", "A"))
+	rrs := r.Resolve("google.com", "A")
 	st.Expect(t, len(rrs) >= 10, true)
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "NS" }) >= 2, true)
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "A" }) >= 4, true)
@@ -47,7 +47,7 @@ func TestGoogleA(t *testing.T) {
 
 func TestGoogleAny(t *testing.T) {
 	r := New(0)
-	rrs := accum(r.Resolve("google.com", ""))
+	rrs := r.Resolve("google.com", "")
 	st.Expect(t, len(rrs) >= 10, true)
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "NS" }) >= 2, true)
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "A" }) >= 4, true)
@@ -55,8 +55,8 @@ func TestGoogleAny(t *testing.T) {
 
 func TestGoogleMulti(t *testing.T) {
 	r := New(0)
-	accum(r.Resolve("google.com", "A"))
-	rrs := accum(r.Resolve("google.com", "TXT"))
+	r.Resolve("google.com", "A")
+	rrs := r.Resolve("google.com", "TXT")
 	st.Expect(t, len(rrs) >= 10, true)
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "TXT" }), 1)
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "A" }) >= 4, true)
@@ -64,57 +64,57 @@ func TestGoogleMulti(t *testing.T) {
 
 func TestGoogleTXT(t *testing.T) {
 	r := New(0)
-	rrs := accum(r.Resolve("google.com", "TXT"))
+	rrs := r.Resolve("google.com", "TXT")
 	st.Expect(t, len(rrs), 5)
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "TXT" }), 1)
 }
 
 func TestHerokuA(t *testing.T) {
 	r := New(0)
-	rrs := accum(r.Resolve("us-east-1-a.route.herokuapp.com", "A"))
+	rrs := r.Resolve("us-east-1-a.route.herokuapp.com", "A")
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "A" }) >= 1, true)
 }
 
 func TestHerokuTXT(t *testing.T) {
 	r := New(0)
-	rrs := accum(r.Resolve("us-east-1-a.route.herokuapp.com", "TXT"))
+	rrs := r.Resolve("us-east-1-a.route.herokuapp.com", "TXT")
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "TXT" }), 0)
 }
 
 func TestHerokuMulti(t *testing.T) {
 	r := New(0)
-	accum(r.Resolve("us-east-1-a.route.herokuapp.com", "A"))
-	rrs := accum(r.Resolve("us-east-1-a.route.herokuapp.com", "TXT"))
+	r.Resolve("us-east-1-a.route.herokuapp.com", "A")
+	rrs := r.Resolve("us-east-1-a.route.herokuapp.com", "TXT")
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "TXT" }), 0)
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "A" }) >= 1, true)
 }
 
 func TestBlueOvenA(t *testing.T) {
 	r := New(0)
-	rrs := accum(r.Resolve("blueoven.com", "A"))
+	rrs := r.Resolve("blueoven.com", "A")
 	st.Expect(t, len(rrs), 2)
 	st.Expect(t, all(rrs, func(rr *RR) bool { return rr.Type == "NS" }), true)
 }
 
 func TestBlueOvenAny(t *testing.T) {
 	r := New(0)
-	rrs := accum(r.Resolve("blueoven.com", ""))
+	rrs := r.Resolve("blueoven.com", "")
 	st.Expect(t, len(rrs), 2)
 	st.Expect(t, all(rrs, func(rr *RR) bool { return rr.Type == "NS" }), true)
 }
 
 func TestBlueOvenMulti(t *testing.T) {
 	r := New(0)
-	accum(r.Resolve("blueoven.com", "A"))
-	accum(r.Resolve("blueoven.com", "TXT"))
-	rrs := accum(r.Resolve("blueoven.com", ""))
+	r.Resolve("blueoven.com", "A")
+	r.Resolve("blueoven.com", "TXT")
+	rrs := r.Resolve("blueoven.com", "")
 	st.Expect(t, len(rrs), 2)
 	st.Expect(t, all(rrs, func(rr *RR) bool { return rr.Type == "NS" }), true)
 }
 
 func TestBazCoUKAny(t *testing.T) {
 	r := New(0)
-	rrs := accum(r.Resolve("baz.co.uk", ""))
+	rrs := r.Resolve("baz.co.uk", "")
 	st.Expect(t, len(rrs) >= 2, true)
 	st.Expect(t, count(rrs, func(rr *RR) bool { return rr.Type == "NS" }) >= 2, true)
 }
@@ -126,14 +126,10 @@ func BenchmarkResolve(b *testing.B) {
 }
 
 func testResolve() {
-	accum(testResolver.Resolve("google.com", ""))
-	accum(testResolver.Resolve("blueoven.com", ""))
-	accum(testResolver.Resolve("baz.co.uk", ""))
-	accum(testResolver.Resolve("us-east-1-a.route.herokuapp.com", ""))
-}
-
-func accum(in []*RR) []*RR {
-	return in
+	testResolver.Resolve("google.com", "")
+	testResolver.Resolve("blueoven.com", "")
+	testResolver.Resolve("baz.co.uk", "")
+	testResolver.Resolve("us-east-1-a.route.herokuapp.com", "")
 }
 
 func count(rrs []*RR, f func(*RR) bool) (out int) {
