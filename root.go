194a3da6d7814224c76a18284fcd6907c96486e4
Domainr Robot <robot@domainr.com>
2018-05-09 08:13:43 +0000

    auto-update
    M  root.go

--- root.go
+++ root.go
@@ -12,8 +12,8 @@ var root = `
 ;           on server           FTP.INTERNIC.NET
 ;       -OR-                    RS.INTERNIC.NET
 ; 
-;       last update:     May 03, 2018 
-;       related version of root zone:     2018050301
+;       last update:     May 08, 2018 
+;       related version of root zone:     2018050801
 ; 
 ; FORMERLY NS.INTERNIC.NET 
 ;
