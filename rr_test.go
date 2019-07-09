17b60632989593d6355735a2b57747359b80550f
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-07-09 18:06:54 -0400

    Test expiring RR string

--- rr_test.go
+++ rr_test.go
@@ -2,6 +2,7 @@ package dnsr
 
 import (
 	"testing"
+	"time"
 
 	"github.com/nbio/st"
 )
@@ -14,4 +15,17 @@ func TestRRStringStandard(t *testing.T) {
 	}
 	result := rr.String()
 	st.Expect(t, result, "example.com.	      3600	IN	A	203.0.113.1")
+}
+
+func TestRRStringExpiring(t *testing.T) {
+	ttl := 86400 * time.Second
+	rr := RR{
+		Name:   "example.com.",
+		Type:   "A",
+		Value:  "203.0.113.1",
+		TTL:    ttl,
+		Expiry: time.Now().Add(ttl),
+	}
+	result := rr.String()
+	st.Expect(t, result, "example.com.	     86400	IN	A	203.0.113.1")
 }
