ffa7a4c785f77aad300b6e06ce02b407e0005b51
Odin Hultgren van der Horst <odihor@one.com>
2024-01-16 12:02:05 +0100

    Fixed so canceled ctx is not propegated into resolveCNAME

--- resolver.go
+++ resolver.go
@@ -180,7 +180,7 @@ func (r *Resolver) resolve(ctx context.Context, qname, qtype string, depth int) 
 func (r *Resolver) iterateParents(ctx context.Context, qname, qtype string, depth int) (RRs, error) {
 	chanRRs := make(chan RRs, MaxNameservers)
 	chanErrs := make(chan error, MaxNameservers)
-	ctx, cancel := context.WithCancel(ctx)
+	child_ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	for pname, ok := qname, true; ok; pname, ok = parent(pname) {
 		// If we’re looking for [foo.com,NS], then move on to the parent ([com,NS])
@@ -195,7 +195,7 @@ func (r *Resolver) iterateParents(ctx context.Context, qname, qtype string, dept
 		}
 
 		// Get nameservers
-		nrrs, err := r.resolve(ctx, pname, "NS", depth)
+		nrrs, err := r.resolve(child_ctx, pname, "NS", depth)
 		if err == NXDOMAIN || err == ErrTimeout || err == context.DeadlineExceeded {
 			return nil, err
 		}
@@ -205,7 +205,7 @@ func (r *Resolver) iterateParents(ctx context.Context, qname, qtype string, dept
 
 		// Check cache for specific queries
 		if len(nrrs) > 0 && qtype != "" {
-			rrs, err := r.cacheGet(ctx, qname, qtype)
+			rrs, err := r.cacheGet(child_ctx, qname, qtype)
 			if err != nil {
 				return nil, err
 			}
@@ -223,7 +223,7 @@ func (r *Resolver) iterateParents(ctx context.Context, qname, qtype string, dept
 			}
 
 			go func(host string) {
-				rrs, err := r.exchange(ctx, host, qname, qtype, depth)
+				rrs, err := r.exchange(child_ctx, host, qname, qtype, depth)
 				if err != nil {
 					chanErrs <- err
 				} else {
@@ -237,8 +237,8 @@ func (r *Resolver) iterateParents(ctx context.Context, qname, qtype string, dept
 		// Wait for answer, error, or cancellation
 		for ; count > 0; count-- {
 			select {
-			case <-ctx.Done():
-				return nil, ctx.Err()
+			case <-child_ctx.Done():
+				return nil, child_ctx.Err()
 			case rrs := <-chanRRs:
 				for _, nrr := range nrrs {
 					if nrr.Name == qname {
