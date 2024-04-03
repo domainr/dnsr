67acfb4cbf763c305f43b7ed62c9854fc5d1e7a9
Cameron Walters <cameron.walters@fastly.com>
2024-04-03 12:53:26 -0700

    resolver_test: update test now that CNAME recurses to A

--- resolver_test.go
+++ resolver_test.go
@@ -175,8 +175,8 @@ func TestGoogleCNAME(t *testing.T) {
 	rrs, err := r.ResolveErr("translate.google.com", "A")
 	st.Expect(t, err, nil)
 	st.Expect(t, len(rrs) >= 1, true)
-	st.Expect(t, count(rrs, func(rr RR) bool { return rr.Type == "A" }), 0)
-	st.Expect(t, count(rrs, func(rr RR) bool { return rr.Type == "CNAME" }), 1)
+	st.Expect(t, count(rrs, func(rr RR) bool { return rr.Type == "CNAME" }), 1)     // resolved first
+	st.Expect(t, count(rrs, func(rr RR) bool { return rr.Type == "A" }) >= 1, true) // records for CNAME target
 }
 
 func TestGoogleTXT(t *testing.T) {
